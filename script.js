bc19bb0b4419638a229dbea509f089f1c44c6947
adesanto <aarondesanto@gmail.com>
2015-11-07 15:13:37 -0500

    Small js update and re-added favicon.

--- script.js
+++ script.js
@@ -2,67 +2,67 @@ var theater = [
 	[
 		{
 			name: "seat1",
-			reserved: false
-		}, 
-		{
-			name: "seat2",
-			reserved: false
-		}, 
-		{
-			name: "seat3",
-			reserved: false
-		}, 
-		{
-			name: "seat4",
-			reserved: false
-		}, 
-		{
-			name: "seat5",
-			reserved: false
-		}, 
-		{
-			name: "seat6",
-			reserved: false
-		}
-	], 
-	[
-		{
-			name: "seat1",
-			reserved: false
-		}, 
-		{
-			name: "seat2",
-			reserved: false
-		}, 
-		{
-			name: "seat3",
-			reserved: false
-		}, 
-		{
-			name: "seat4",
-			reserved: false
-		}, 
-		{
-			name: "seat5",
-			reserved: false
-		}, 
-		{
-			name: "seat6",
-			reserved: false
-		}
-	], 
-	[
-		{
-			name: "seat1",
-			reserved: false
-		}, 
-		{
-			name: "seat2",
-			reserved: false
-		}, 
-		{
-			name: "seat3",
 			reserved: true
+		}, 
+		{
+			name: "seat2",
+			reserved: false
+		}, 
+		{
+			name: "seat3",
+			reserved: false
+		}, 
+		{
+			name: "seat4",
+			reserved: false
+		}, 
+		{
+			name: "seat5",
+			reserved: false
+		}, 
+		{
+			name: "seat6",
+			reserved: false
+		}
+	], 
+	[
+		{
+			name: "seat1",
+			reserved: false
+		}, 
+		{
+			name: "seat2",
+			reserved: false
+		}, 
+		{
+			name: "seat3",
+			reserved: false
+		}, 
+		{
+			name: "seat4",
+			reserved: false
+		}, 
+		{
+			name: "seat5",
+			reserved: false
+		}, 
+		{
+			name: "seat6",
+			reserved: false
+		}
+	], 
+	[
+		{
+			name: "seat1",
+			reserved: false
+		}, 
+		{
+			name: "seat2",
+			reserved: false
+		}, 
+		{
+			name: "seat3",
+			reserved: false
 		}, 
 		{
 			name: "seat4",
@@ -105,28 +105,17 @@ var theater = [
 	]
 ]
 
-console.log(theater[0][0].name);
-console.log(theater[0][0].reserved);
-
 function isSeatReserved(rowNum, seatNum) {
 	if (theater[rowNum][seatNum].reserved === true) {
-		seatNumberToId(rowNum, seatNum);
+		document.getElementById(seatNumberToId(rowNum, seatNum)).style.backgroundColor = "#991111";
 	} else {
 		console.log("Free");
 	}
 }
 
+function seatNumberToId(rowNum, seatNum) {
+	return ("r" + rowNum + "s" + seatNum);
+}
 
-
-isSeatReserved(2, 2);
-
-// var r1s6 = document.getElementById('r1s6');
-
-// document.getElementById('r1s6').style.backgroundColor = "Blue";
-
-// if (seatReserved === true) {
-	// document.getElementById(seatID).style.backgroundColor = "Red";
-// }
-
-
+isSeatReserved(0, 0);
 
