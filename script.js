5a6299303e7944150d2f04351f4dbfc59e8d0219
adesanto <aarondesanto@gmail.com>
2015-11-07 17:20:59 -0500

    Form and thank you message JS is functional, CSS needed

--- script.js
+++ script.js
@@ -1,121 +1,165 @@
 var theater = [
-	[
+	[ // Row One
 		{
 			name: "seat1",
-			reserved: true
+			reserved: true,
+			cost:  8
 		}, 
 		{
 			name: "seat2",
-			reserved: false
+			reserved: false,
+			cost:  8
 		}, 
 		{
 			name: "seat3",
-			reserved: false
+			reserved: false,
+			cost:  8
 		}, 
 		{
 			name: "seat4",
-			reserved: false
+			reserved: false,
+			cost:  8
 		}, 
 		{
 			name: "seat5",
-			reserved: false
+			reserved: false,
+			cost:  8
 		}, 
 		{
 			name: "seat6",
-			reserved: false
+			reserved: false,
+			cost:  8
 		}
 	], 
-	[
+	[ // Row Two
 		{
 			name: "seat1",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat2",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat3",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat4",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat5",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat6",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}
 	], 
-	[
+	[ // Row Three
 		{
 			name: "seat1",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat2",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat3",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat4",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat5",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat6",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}
 	], 
-	[
+	[ // Row Four
 		{
 			name: "seat1",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat2",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat3",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat4",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat5",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}, 
 		{
 			name: "seat6",
-			reserved: false
+			reserved: false,
+			cost:  5
 		}
 	]
 ]
 
+var reserveButton = document.querySelector("#inputReserve");
+var reserveAnother = document.querySelector("#inputReserveAnother");
+var reserveForm = document.querySelector("#seatIsReserved");
+
 function isSeatReserved(rowNum, seatNum) {
 	if (theater[rowNum][seatNum].reserved === true) {
-		document.getElementById(seatNumberToId(rowNum, seatNum)).style.backgroundColor = "#991111";
+		makeSeatRed(rowNum, seatNum);
 	} else {
 		console.log("Free");
 	}
 }
 
-function seatNumberToId(rowNum, seatNum) {
-	return ("r" + rowNum + "s" + seatNum);
+function makeSeatRed(rowNum, seatNum) {
+	return document.querySelector(seatNumberToId(rowNum, seatNum)).style.backgroundColor = "#991111";
 }
+
+function seatNumberToId(rowNum, seatNum) {
+	return ("#r" + rowNum + "s" + seatNum);
+}
+
+// This function submits the reservation form and then hides it and shows the thank you message
+reserveButton.addEventListener('click', function() {
+	return (document.querySelector("#formContainer").style.display = "none") && (document.querySelector("#thankYou").style.display = "block");
+});
+
+// This function hides the thank you message and shows the reservation form again if the user wishes to make multiple reservations
+reserveAnother.addEventListener('click', function() {
+	return (document.querySelector("#formContainer").style.display = "block") && (document.querySelector("#thankYou").style.display = "none");
+});
+
+
 
 isSeatReserved(0, 0);
 
