50a6bd98119734b3e9fd6e880b256d6c6c323bb9
adesanto <aarondesanto@gmail.com>
2015-11-09 14:59:30 -0500

    wtf is git doing

--- script.js
+++ script.js
@@ -1,3 +1,174 @@
-$(document).ready(function() {
-	$('#r0s0').css('backgroundColor', '#660000');
+var theater = [
+	[ // Row Zero
+		{
+			name: "seat1",
+			reserved: true,
+			cost:  8
+		}, 
+		{
+			name: "seat2",
+			reserved: true,
+			cost:  8
+		}, 
+		{
+			name: "seat3",
+			reserved: true,
+			cost:  8
+		}, 
+		{
+			name: "seat4",
+			reserved: true,
+			cost:  8
+		}, 
+		{
+			name: "seat5",
+			reserved: true,
+			cost:  8
+		}, 
+		{
+			name: "seat6",
+			reserved: false,
+			cost:  8
+		}
+	], 
+	[ // Row One
+		{
+			name: "seat1",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat2",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat3",
+			reserved: true,
+			cost:  5
+		}, 
+		{
+			name: "seat4",
+			reserved: true,
+			cost:  5
+		}, 
+		{
+			name: "seat5",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat6",
+			reserved: false,
+			cost:  5
+		}
+	], 
+	[ // Row Two
+		{
+			name: "seat1",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat2",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat3",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat4",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat5",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat6",
+			reserved: false,
+			cost:  5
+		}
+	], 
+	[ // Row Three
+		{
+			name: "seat1",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat2",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat3",
+			reserved: true,
+			cost:  5
+		}, 
+		{
+			name: "seat4",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat5",
+			reserved: false,
+			cost:  5
+		}, 
+		{
+			name: "seat6",
+			reserved: false,
+			cost:  5
+		}
+	]
+]
+
+var reserveButton = document.querySelector("#inputReserve");
+var reserveAnother = document.querySelector("#inputReserveAnother");
+var reserveForm = document.querySelector("#reserveForm");
+
+function isSeatReserved(rowNum, seatNum) {
+	if (theater[rowNum][seatNum].reserved === true) {
+		makeSeatRed(rowNum, seatNum);
+	} else {
+		console.log("Free");
+	}
+}
+
+function makeSeatRed(rowNum, seatNum) {
+	return document.querySelector(seatNumberToId(rowNum, seatNum)).style.backgroundColor = "#5B2025";
+}
+
+function seatNumberToId(rowNum, seatNum) {
+	return ("#r" + rowNum + "s" + seatNum);
+}
+
+function appSeatID(ID) {
+	var seatChosen = document.querySelector("#reserveFormPara");
+	seatChosen.innerHTML = "You have chosen seat: " + ID;
+}
+
+// This event listener submits the reservation form and then hides it and shows the thank you message
+reserveButton.addEventListener('click', function() {
+	return (reserveForm.style.display = "none") && (document.querySelector("#thankYou").style.display = "block");
 });
+
+// This event listener hides the thank you message and shows the reservation form again if the user wishes to make multiple reservations
+reserveAnother.addEventListener('click', function() {
+	return (reserveForm.style.display = "block") && (document.querySelector("#thankYou").style.display = "none");
+});
+
+isSeatReserved(0, 0);
+isSeatReserved(0, 3);
+isSeatReserved(0, 2);
+isSeatReserved(0, 4);
+isSeatReserved(0, 1);
+isSeatReserved(1, 2);
+isSeatReserved(1, 3);
+isSeatReserved(3, 2);
