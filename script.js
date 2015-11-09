d02ccb21edc495e2785021679cada6f9752d55a3
PSalatrik <philip.salatrik@gmail.com>
2015-11-09 15:22:52 -0500

    jquery

--- script.js
+++ script.js
@@ -172,3 +172,51 @@ isSeatReserved(0, 1);
 isSeatReserved(1, 2);
 isSeatReserved(1, 3);
 isSeatReserved(3, 2);
+
+
+$(document).ready(function(){
+
+  //Exercise 1
+
+  $('img').mouseenter(function(){
+    $('img').fadeTo('fast', 0.5);
+  });
+  $('img').mouseleave(function(){
+    $('img').fadeTo('fast', 1);
+  });
+
+
+  //Exercise 2
+  //2.1
+  $('.fade-button').click(function(){
+    $('div').fadeOut();
+  });
+
+  //2.2
+  $('.fade-button').click(function(){
+    $('#fader').fadeOut();
+  });
+
+  //Bonus
+  $('.box').click(function(){
+    $(this).fadeOut();
+  });
+
+
+  //Exercise 3
+  $('.longbox').mouseenter(function(){
+    $(this).fadeTo('fast', 0.25);
+  });
+
+  $('.longbox').mouseleave(function(){
+    $(this).fadeTo('fast', 1);
+  });
+
+  $('.longbox').click(function(){
+    $(this).toggleClass('off-white');
+    $(this).toggleClass('black-text');
+    $(this).text('clicked!');
+  });
+
+
+});
