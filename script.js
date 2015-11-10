3a613529de1256d7985e28b9fffa19b491e7ec20
adesanto <aarondesanto@gmail.com>
2015-11-10 10:24:41 -0500

    Seating info (properties name, ccn, and email) can now be accessed using only a seat <div>s ID

--- script.js
+++ script.js
@@ -1,51 +1,99 @@
 var theater = {
 	reserved: ["#r0s0", "#r0s1", "#r0s2", "#r0s3", "#r0s4", "#r1s2", "#r1s3", "#r3s2"],
-	userinfo: {
-		r0s0: {
+	userinfo: [
+		{
+			id: "#r0s0",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r0s1: {
+		{
+			id: "#r0s1",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r0s2: {
+		{
+			id: "#r0s2",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r0s3: {
+		{
+			id: "#r0s3",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r0s4: {
+		{
+			id: "#r0s4",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r1s2: {
+		{
+			id: "#r1s2",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r1s3: {
+		{
+			id: "#r1s3",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		},
-		r3s2: {
+		{
+			id: "#r3s2",
 			name: "User Name",
 			ccn: "1234567890123456",
 			email: "user@example.com"
 		}
-	}
+	]
 }
 
-console.log(theater.reserved[3]);
-console.log(theater.userinfo.r0s3.ccn);
+var arr1LengthMinusOne = "";
+var arr2LengthMinusOne = "";
+
+console.log(theater.reserved[1]);
+console.log(theater.userinfo[1].name);
+console.log(theater.userinfo[1].ccn);
+console.log(theater.userinfo[1].email);
+console.log(theater.userinfo[1].ID);
+
+function addReservation(seatId, uName, uCcn, uEmail) {
+	theater.userinfo.push( { name: uName, ccn: uCcn, email: uEmail, id: seatId} );
+	theater.reserved[theater.reserved.length] = seatId;
+	arr1LengthMinusOne = (theater.userinfo.length) - 1;
+	arr2LengthMinusOne = (theater.userinfo.length) - 1;
+}
+
+function showResStatus(seatId) {
+	var seatPosition = theater.reserved.indexOf(seatId);
+	console.log(seatPosition);
+	console.log(theater.userinfo[seatPosition].name);
+	console.log(theater.userinfo[seatPosition].ccn);
+	console.log(theater.userinfo[seatPosition].email);
+}
+
+addReservation("#r2s2", "User Name", "5555", "user@eample.com");
+
+console.log(theater.reserved[arr2LengthMinusOne]);
+console.log(theater.userinfo[arr1LengthMinusOne].name);
+console.log(theater.userinfo[arr1LengthMinusOne].ccn);
+console.log(theater.userinfo[arr1LengthMinusOne].email);
+console.log(theater.userinfo[arr1LengthMinusOne].ID);
+
+addReservation("#r2s3", "Name User", "4444", "user@xample.com");
+
+console.log(theater.reserved[arr2LengthMinusOne]);
+console.log(theater.userinfo[arr1LengthMinusOne].name);
+console.log(theater.userinfo[arr1LengthMinusOne].ccn);
+console.log(theater.userinfo[arr1LengthMinusOne].email);
+console.log(theater.userinfo[arr1LengthMinusOne].ID);
+
+showResStatus("#r2s2");
+showResStatus("#r2s3");
 
 // $(document).ready(function(){
 
