a751b36d52f4ce5f8a4efe208fe08c2997bf53cb
adesanto <aarondesanto@gmail.com>
2015-11-10 09:24:30 -0500

    Data structure for storing user info and reserved seating is up and running.

--- script.js
+++ script.js
@@ -1,181 +1,53 @@
-var theater = [
-	[ // Row Zero
-		{
-			name: "seat1",
-			reserved: true,
-			cost:  8
-		}, 
-		{
-			name: "seat2",
-			reserved: true,
-			cost:  8
-		}, 
-		{
-			name: "seat3",
-			reserved: true,
-			cost:  8
-		}, 
-		{
-			name: "seat4",
-			reserved: true,
-			cost:  8
-		}, 
-		{
-			name: "seat5",
-			reserved: true,
-			cost:  8
-		}, 
-		{
-			name: "seat6",
-			reserved: false,
-			cost:  8
+var theater = {
+	reserved: ["#r0s0", "#r0s1", "#r0s2", "#r0s3", "#r0s4", "#r1s2", "#r1s3", "#r3s2"],
+	userinfo: {
+		r0s0: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r0s1: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r0s2: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r0s3: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r0s4: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r1s2: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r1s3: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
+		},
+		r3s2: {
+			name: "User Name",
+			ccn: "1234567890123456",
+			email: "user@example.com"
 		}
-	], 
-	[ // Row One
-		{
-			name: "seat1",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat2",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat3",
-			reserved: true,
-			cost:  5
-		}, 
-		{
-			name: "seat4",
-			reserved: true,
-			cost:  5
-		}, 
-		{
-			name: "seat5",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat6",
-			reserved: false,
-			cost:  5
-		}
-	], 
-	[ // Row Two
-		{
-			name: "seat1",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat2",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat3",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat4",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat5",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat6",
-			reserved: false,
-			cost:  5
-		}
-	], 
-	[ // Row Three
-		{
-			name: "seat1",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat2",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat3",
-			reserved: true,
-			cost:  5
-		}, 
-		{
-			name: "seat4",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat5",
-			reserved: false,
-			cost:  5
-		}, 
-		{
-			name: "seat6",
-			reserved: false,
-			cost:  5
-		}
-	]
-]
-
-var newTestVar = 0;
-var reserveButton = document.querySelector("#inputReserve");
-var reserveAnother = document.querySelector("#inputReserveAnother");
-var reserveForm = document.querySelector("#reserveForm");
-
-function isSeatReserved(rowNum, seatNum) {
-	if (theater[rowNum][seatNum].reserved === true) {
-		makeSeatRed(rowNum, seatNum);
-	} else {
-		console.log("Free");
 	}
 }
 
-function makeSeatRed(rowNum, seatNum) {
-	return document.querySelector(seatNumberToId(rowNum, seatNum)).style.backgroundColor = "#5B2025";
-}
+console.log(theater.reserved[3]);
+console.log(theater.userinfo.r0s3.ccn);
 
-function seatNumberToId(rowNum, seatNum) {
-	return ("#r" + rowNum + "s" + seatNum);
-}
-
-function appSeatID(ID) {
-	var seatChosen = document.querySelector("#reserveFormPara");
-	seatChosen.innerHTML = "You have chosen seat: " + ID;
-}
-
-// This event listener submits the reservation form and then hides it and shows the thank you message
-reserveButton.addEventListener('click', function() {
-	return (reserveForm.style.display = "none") && (document.querySelector("#thankYou").style.display = "block");
-});
-
-// This event listener hides the thank you message and shows the reservation form again if the user wishes to make multiple reservations
-reserveAnother.addEventListener('click', function() {
-	return (reserveForm.style.display = "block") && (document.querySelector("#thankYou").style.display = "none");
-});
-
-isSeatReserved(0, 0);
-isSeatReserved(0, 3);
-isSeatReserved(0, 2);
-isSeatReserved(0, 4);
-isSeatReserved(0, 1);
-isSeatReserved(1, 2);
-isSeatReserved(1, 3);
-isSeatReserved(3, 2);
-
-
-$(document).ready(function(){
+// $(document).ready(function(){
 
 //Exercise 1
 //  $('img').mouseenter(function(){
